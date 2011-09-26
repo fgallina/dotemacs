5a81899d141fa2e25f7bf7579ee7501fc2c0f76b
Fabián Ezequiel Gallina <fgallina@cuca>
2011-09-26 15:32:17 -0300

    custom startup enhancement

--- init.el
+++ init.el
@@ -2,9 +2,8 @@
 (let ((default-directory "~/.emacs.d/vendor"))
   (normal-top-level-add-subdirs-to-load-path))
 
-(ignore-errors
-  (load-file "~/.emacs.d/secrets.el")
-  (load-file "~/.emacs.d/pre-startup.el"))
+(ignore-errors (load-file "~/.emacs.d/secrets.el"))
+(ignore-errors (load-file "~/.emacs.d/pre-startup.el"))
 
 (require 'startupd)
 (startupd-load-files)
