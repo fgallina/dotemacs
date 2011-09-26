36ce1126312fd775173ecdc1599f8f358efd9f5a
Fabián Ezequiel Gallina <fgallina@cuca>
2011-09-26 15:25:42 -0300

    Enhanced startup customization by adding support for pre-startup.el and post-startup.el files.

--- init.el
+++ init.el
@@ -2,12 +2,14 @@
 (let ((default-directory "~/.emacs.d/vendor"))
   (normal-top-level-add-subdirs-to-load-path))
 
-(ignore-errors (load-file "~/.emacs.d/secrets.el"))
+(ignore-errors
+  (load-file "~/.emacs.d/secrets.el")
+  (load-file "~/.emacs.d/pre-startup.el"))
 
 (require 'startupd)
 (startupd-load-files)
 
-(ignore-errors (load-file "~/.emacs.d/overrides.el"))
+(ignore-errors (load-file "~/.emacs.d/post-startup.el"))
 
 (setq custom-file "~/.emacs.d/customizations.el")
 (load custom-file 'noerror)
