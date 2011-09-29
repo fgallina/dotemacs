34e1ad350f56379f7f1d0f142ca5a10db6958d8f
Fabián Ezequiel Gallina <fgallina@cuca>
2011-09-28 22:25:46 -0300

    Simplify smart-tab setup

--- startup.d/40smart-tab.el
+++ startup.d/40smart-tab.el
@@ -17,37 +17,4 @@
 
 (setq smart-tab-using-hippie-expand t)
 
-(defun smart-tab-call-completion-function ()
-  "Get a completion function according to current major mode."
-  (let ((completion-function
-         (cdr (assq major-mode smart-tab-completion-functions-alist))))
-    (if (null completion-function)
-        (if (and (not (minibufferp))
-                 (memq 'auto-complete-mode minor-mode-list)
-                 auto-complete-mode)
-            (auto-complete)
-          (if smart-tab-using-hippie-expand
-              (hippie-expand nil)
-            (dabbrev-expand nil)))
-      (funcall completion-function))))
-
-;;;###autoload
-(defun smart-tab (&optional prefix)
-  "Try to 'do the smart thing' when tab is pressed.
-`smart-tab' attempts to expand the text before the point or
-indent the current line or selection.
-
-In a regular buffer, `smart-tab' will attempt to expand with
-either `hippie-expand' or `dabbrev-expand', depending on the
-value of `smart-tab-using-hippie-expand'. Alternatively, if
-`auto-complete-mode' is enabled in the current buffer,
-`auto-complete' will be used to attempt expansion. If the mark is
-active, or PREFIX is \\[universal-argument], then `smart-tab'
-will indent the region or the current line (if the mark is not
-active)."
-  (interactive "P")
-  (if (smart-tab-must-expand prefix)
-      (smart-tab-call-completion-function)
-    (smart-tab-default)))
-
 (global-smart-tab-mode 1)
