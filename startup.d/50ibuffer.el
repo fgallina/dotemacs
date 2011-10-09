8be5de743e2439798b801cc1e39ea7f610e2b7bf
Fabián Ezequiel Gallina <fgallina@cuca>
2011-10-08 21:15:27 -0300

    Added lua-mode to ibuffer listing

--- startup.d/50ibuffer.el
+++ startup.d/50ibuffer.el
@@ -1,12 +1,13 @@
 (setq ibuffer-saved-filter-groups
       (quote (("default"
 	       ("python" (mode . python-mode))
+	       ("elisp" (mode . emacs-lisp-mode))
+	       ("lua" (mode . lua-mode))
 	       ("javascript" (mode . js-mode))
 	       ("html" (mode . html-mode))
 	       ("css" (mode . css-mode))
 	       ("irc" (mode . rcirc-mode))
 	       ("term" (mode . term-mode))
-	       ("elisp" (mode . emacs-lisp-mode))
 	       ("magit" (name . "^\\*magit"))
 	       ("emacs" (or
 			 (name . "^\\*scratch\\*$")
