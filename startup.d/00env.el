414c9358c2ab5e6ad5a42023c7a672bc7c605fa4
Fabián Ezequiel Gallina <fgallina@cuca>
2011-12-17 20:24:24 -0300

    Added ~/bin to the PATH

--- startup.d/00env.el
+++ startup.d/00env.el
@@ -1,3 +1,6 @@
-(let ((bindir (expand-file-name "~/.emacs.d/bin")))
-  (setenv "PATH" (concat bindir path-separator (getenv "PATH")))
-  (add-to-list 'exec-path bindir))
+(let ((bindirs (list
+                (expand-file-name "~/.emacs.d/bin")
+                (expand-file-name "~/bin"))))
+  (dolist (dir bindirs)
+    (setenv "PATH" (concat dir path-separator (getenv "PATH")))
+    (add-to-list 'exec-path dir)))
