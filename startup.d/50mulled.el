e83a6d943cbe3548028286ce7401e5e527bd543c
Fabián Ezequiel Gallina <fgallina@cuca>
2012-07-19 22:34:59 -0300

    Removed pastebin and mulled submodules

--- startup.d/50mulled.el
+++
@@ -1,3 +0,0 @@
-(require 'multiple-line-edit)
-(global-set-key (kbd "M-g a") 'mulled/edit-leading-edges)
-(global-set-key (kbd "M-g e") 'mulled/edit-trailing-edges)
--- startup.d/50pastebin.el
+++
@@ -1,4 +0,0 @@
-(autoload 'pastebin "pastebin"
-  "pastes the contents of a buffer or region to pastebin" t)
-(setq pastebin-default-expire-date "1D")
-(setq pastebin-default-private 1)
