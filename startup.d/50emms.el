80ee9c00d6c7b6bc0bd81fbc5cf7965fc9d2981a
Fabián Ezequiel Gallina <fgallina@cuca>
2012-08-17 20:13:16 -0300

    Enhancements to emms initialization.

--- startup.d/50emms.el
+++ startup.d/50emms.el
@@ -5,14 +5,14 @@
 
 ;;; From http://www.gnu.org/software/emms/configs/lb-emms.el
 ;; play what i mean
-(defun emms-pwim (truc &optional rien)
+(defun my-emms-pwim (truc &optional rien)
   "Plays the TRUC specified, whatever it is. The function tries to
 guess the type of TRUC, between playlist, directory containing
 playable tracks, and files. If the directory does not contain
 playable tracks, but some sub-directories, it will play the
 tree."
   (interactive
-   (find-file-read-args "Play what ? " t))
+   (find-file-read-args "Play what? " t))
   (cond
    ((file-directory-p truc)
     (emms-play-directory-tree truc))
@@ -20,40 +20,47 @@ tree."
     (emms-play-file truc))))
 
 ;; add what i mean
-(defun emms-awim (truc &optional rien)
+(defun my-emms-awim (truc &optional rien)
   "Adds the TRUC specified, whatever it is. The function tries to
 guess the type of TRUC, between playlist, directory containing
 playable tracks, and files. If the directory does not contain
 playable tracks, but some sub-directories, it will add the
 tree."
   (interactive
-   (find-file-read-args "Add what ? " t))
+   (find-file-read-args "Add what? " t))
   (cond
    ((file-directory-p truc)
     (emms-add-directory-tree truc))
    ((file-exists-p truc)
     (emms-add-file truc))))
 
+(defun my-emms ()
+  "Same as `emms' but use `my-emms-pwim' at startup."
+  (interactive)
+  (if (or (null emms-playlist-buffer)
+          (not (buffer-live-p emms-playlist-buffer)))
+      (call-interactively 'my-emms-pwim))
+  (emms-playlist-mode-go))
+
 ;; Libtag support
 (require 'emms-info-libtag)
-(setq emms-info-functions '(emms-info-libtag))
 
 ;; Stolen and adapted from TWB
 (defun my-emms-info-track-description (track)
   "Return a description of the current track."
-  (if (and (emms-track-get track 'info-artist)
-           (emms-track-get track 'info-title))
-      (let ((pmin (emms-track-get track 'info-playing-time-min))
-            (psec (emms-track-get track 'info-playing-time-sec))
-            (ptot (emms-track-get track 'info-playing-time))
-            (art  (emms-track-get track 'info-artist))
-            (tit  (emms-track-get track 'info-title)))
-        (cond ((and pmin psec) (format "%s - %s [%02d:%02d]" art tit pmin psec))
-              (ptot (format  "%s - %s [%02d:%02d]" art tit (/ ptot 60) (% ptot 60)))
-              (t (emms-track-simple-description track))))
-    (let ((name (emms-track-name (emms-playlist-current-selected-track))))
-      (when name
-        (file-name-nondirectory name)))))
+  (if (not (eq (emms-track-type track) 'file))
+      (emms-track-simple-description track)
+    (let* ((ptot (emms-track-get track 'info-playing-time))
+           (pmin (or
+                  (emms-track-get track 'info-playing-time-min)
+                  (and ptot (/ ptot 60)) 0))
+           (psec (or
+                  (emms-track-get track 'info-playing-time-sec)
+                  (and ptot (% ptot 60)) 0))
+           (artist (or (emms-track-get track 'info-artist) "unknown"))
+           (title (or (emms-track-get track 'info-title)
+                      (file-name-nondirectory (emms-track-get track 'name)))))
+      (format "%s - %s [%02d:%02d]" artist title pmin psec))))
 
 (setq emms-track-description-function 'my-emms-info-track-description)
 
@@ -62,10 +69,16 @@ tree."
  emms-info-functions '(emms-info-libtag)
  later-do-interval 0.0001
  emms-mode-line-format " %s "
- emms-show-format "NP: %s")
+ emms-show-format "NP: %s"
+ emms-info-libtag-program-name "~/.emacs.d/bin/emms-print-metadata")
 
+(emms-cache -1)
+(emms-mode-line 1)
+(emms-playing-time -1)
 
-(define-key emms-playlist-mode-map "A" 'emms-awim)
-(define-key emms-playlist-mode-map "P" 'emms-pwim)
-
-(global-set-key (kbd "C-x m") 'emms)
+(define-key emms-playlist-mode-map "A" 'my-emms-awim)
+(define-key emms-playlist-mode-map "P" 'my-emms-pwim)
+(define-key emms-playlist-mode-map "S" 'emms-streams)
+(define-key emms-stream-mode-map "q" 'emms)
+(define-key emms-stream-mode-map "Q" 'emms)
+(global-set-key (kbd "C-x m") 'my-emms)
