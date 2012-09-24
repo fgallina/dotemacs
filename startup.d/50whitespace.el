b46feeba7dfe7da275076c15ca90572893fb3b3e
Fabián Ezequiel Gallina <fgallina@cuca>
2012-09-24 16:28:52 -0300

    Remove line tails highligh of whitespace.el

--- startup.d/50whitespace.el
+++ startup.d/50whitespace.el
@@ -1,5 +1,5 @@
 (require 'whitespace)
-(set 'whitespace-style '(trailing tabs lines-tail indentation::space face))
+(setq whitespace-style '(trailing tabs indentation::space face))
 (setq whitespace-global-modes
       '(c-mode c++-mode clojure-mode emacs-lisp-mode js-mode php-mode
                python-mode lisp-mode))
