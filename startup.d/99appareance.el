857ab138029f0aa33581e42fa781d816e4f1562a
Fabián Ezequiel Gallina <fgallina@cuca>
2012-10-02 23:09:51 -0300

    Simplified global config and separated appareance settings from it.

---
+++ startup.d/99appareance.el
@@ -0,0 +1,17 @@
+(and (boundp 'custom-safe-themes)
+     (load-theme 'deeper-blue t))
+
+(set-face-attribute 'default nil :height 105)
+
+(setq-default
+ frame-title-format
+ '(:eval
+   (format "%s@%s:%s"
+           (or (file-remote-p default-directory 'user) user-login-name)
+           (or (file-remote-p default-directory 'host) system-name)
+           (or (buffer-file-name)
+               (and (member (current-buffer) (emms-playlist-buffer-list))
+                    (emms-playlist-current-selected-track)
+                    (emms-track-description
+                     (emms-playlist-current-selected-track)))
+               (buffer-name)))))
--- startup.d/99global.el
+++ startup.d/99global.el
@@ -1,7 +1,7 @@
-(windmove-default-keybindings)
+(require 'uniquify)
+
 (winner-mode 1)
 (savehist-mode 1)
-(fset 'yes-or-no-p 'y-or-n-p)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode 1)
@@ -10,56 +10,26 @@
 (show-paren-mode 1)
 (line-number-mode 1)
 (column-number-mode 1)
-(setq visible-bell t)
-(setq ido-enable-flex-matching t)
-(column-number-mode 1)
-(setq-default
- frame-title-format
- '(:eval
-   (format "%s@%s:%s"
-           (or (file-remote-p default-directory 'user) user-login-name)
-           (or (file-remote-p default-directory 'host) system-name)
-           (or (buffer-file-name)
-               (and (member (current-buffer) (emms-playlist-buffer-list))
-                    (emms-playlist-current-selected-track)
-                    (emms-track-description
-                     (emms-playlist-current-selected-track)))
-               (buffer-name)))))
+
+(windmove-default-keybindings)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq auto-save-default nil)
+(setq warning-suppress-types nil)
+(setq max-specpdl-size 999999999)
+(setq enable-recursive-minibuffers t)
+(setq redisplay-dont-pause t)
 (setq-default indent-tabs-mode nil)
 (setq x-select-enable-clipboard t)
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message "")
 (setq make-backup-files nil)
-(require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
+(setq visible-bell t)
+(setq ido-enable-flex-matching t)
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq auto-save-default nil)
-
-(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
-(setq warning-suppress-types nil)
-
-(defadvice flymake-start-syntax-check-process
-  (after my-flymake-start-syntax-check-process
-         (cmd args dir) activate compile)
-  ;; set flag to allow exit without query on any active flymake
-  ;; processes
-  (set-process-query-on-exit-flag ad-return-value nil))
-
-;; (add-hook 'find-file-hook (lambda ()
-;;                             (when (and buffer-file-name
-;;                                        (not (file-writable-p buffer-file-name))
-;;                                        (y-or-n-p (format "File %s  is read-only. Open it as root?"
-;;                                                          buffer-file-name)))
-;;                               (sudo))))
-
-(set-face-attribute 'default nil :height 105)
-
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-(setq max-specpdl-size 999999999)
-
-(setq enable-recursive-minibuffers t)
-(follow-mode t)
-(setq redisplay-dont-pause t)
+(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
