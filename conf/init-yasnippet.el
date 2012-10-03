5a59c2979a99176859e7fd4d32292250d016239d
Fabián Ezequiel Gallina <fgallina@cuca>
2012-10-03 19:47:48 -0300

    Yasnippet: do not activate in term-mode.

--- conf/init-yasnippet.el
+++ conf/init-yasnippet.el
@@ -1,2 +1,7 @@
-(setq yas-snippet-dirs '("~/.emacs.d/vendor/yasnippet/snippets" "~/.emacs.d/snippets"))
+(setq yas-snippet-dirs '("~/.emacs.d/el-get/yasnippet/snippets" "~/.emacs.d/snippets"))
 (yas-global-mode 1)
+
+(defun yas-not-activate ()
+  (memq major-mode '(term-mode)))
+
+(set-default 'yas--dont-activate (cons #'yas-not-activate yas--dont-activate))
