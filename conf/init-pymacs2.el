d1464108353ca59d60cbe23944aede2b1a9a2010
Fabián Ezequiel Gallina <fgallina@cuca>
2012-10-02 23:08:20 -0300

    Added pymacs config

---
+++ conf/init-pymacs2.el
@@ -0,0 +1,5 @@
+(setenv "PYMACS_PYTHON" "python2")
+(el-get-envpath-prepend "PYTHONPATH" (expand-file-name "~/.emacs.d/el-get/pymacs2/"))
+(el-get-envpath-prepend "PYTHONPATH" (expand-file-name "~/.emacs.d/el-get/rope/"))
+(el-get-envpath-prepend "PYTHONPATH" (expand-file-name "~/.emacs.d/el-get/ropemode/"))
+(el-get-envpath-prepend "PYTHONPATH" (expand-file-name "~/.emacs.d/el-get/ropemacs/"))
