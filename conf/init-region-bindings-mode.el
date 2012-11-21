e8b27cb875e6fb730c361036f3725aa9d3e15e3a
Fabián Ezequiel Gallina <fgallina@cuca>
2012-11-21 19:44:20 -0300

    Disable region-bindings-mode when in term-mode or in a read-only buffer.

--- conf/init-region-bindings-mode.el
+++ conf/init-region-bindings-mode.el
@@ -4,4 +4,9 @@
 (define-key region-bindings-mode-map "P" 'mc/unmark-previous-like-this)
 (define-key region-bindings-mode-map "N" 'mc/unmark-next-like-this)
 (define-key region-bindings-mode-map "m" 'mc/mark-more-like-this-extended)
+(setq region-bindings-mode-disabled-modes '(term-mode))
+(setq region-bindings-mode-disable-predicates
+      (list
+       (lambda ()
+         buffer-read-only)))
 (region-bindings-mode-enable)
