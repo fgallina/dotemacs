1d9c8b87356f46df4a65811e2a5f2144426048d7
Fabián Ezequiel Gallina <galli.87@gmail.com>
2013-03-08 11:16:33 -0300

    Set term-color-* faces if they are defined.

--- startup.d/50terminal.el
+++ startup.d/50terminal.el
@@ -1,36 +1,37 @@
 (require 'term)
 
-(set-face-attribute
- 'term-color-black nil
- :foreground "#282A2E" :background "#373B41") ; 0 8
+(when (facep 'term-color-black)
+  (set-face-attribute
+   'term-color-black nil
+   :foreground "#282A2E" :background "#373B41") ; 0 8
 
-(set-face-attribute
- 'term-color-red nil
- :foreground "#A54242" :background "#CC6666") ; 1 9
+  (set-face-attribute
+   'term-color-red nil
+   :foreground "#A54242" :background "#CC6666") ; 1 9
 
-(set-face-attribute
- 'term-color-green nil
- :foreground "#B5BD68" :background "#8C9440") ; 2 10
+  (set-face-attribute
+   'term-color-green nil
+   :foreground "#B5BD68" :background "#8C9440") ; 2 10
 
-(set-face-attribute
- 'term-color-yellow nil
- :foreground "#DE935F" :background "#f0c674") ; 3 11
+  (set-face-attribute
+   'term-color-yellow nil
+   :foreground "#DE935F" :background "#f0c674") ; 3 11
 
-(set-face-attribute
- 'term-color-blue nil
- :foreground "#81A2BE" :background "#5F819D") ; 4 12
+  (set-face-attribute
+   'term-color-blue nil
+   :foreground "#81A2BE" :background "#5F819D") ; 4 12
 
-(set-face-attribute
- 'term-color-magenta nil
- :foreground "#85678F" :background "#B294BB") ; 5 13
+  (set-face-attribute
+   'term-color-magenta nil
+   :foreground "#85678F" :background "#B294BB") ; 5 13
 
-(set-face-attribute
- 'term-color-cyan nil
- :foreground "#5E8D87" :background "#8ABEB7") ; 6 14
+  (set-face-attribute
+   'term-color-cyan nil
+   :foreground "#5E8D87" :background "#8ABEB7") ; 6 14
 
-(set-face-attribute
- 'term-color-white nil
- :foreground "#C5C8C6" :background "#E8E8D3") ; 7 15
+  (set-face-attribute
+   'term-color-white nil
+   :foreground "#C5C8C6" :background "#E8E8D3")) ; 7 15
 
 ;; Based on:
 ;; http://www.enigmacurry.com/2008/12/26/emacs-ansi-term-tricks/
