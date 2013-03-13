fffbb201e430c7acb3a6ce086e474bf59ce4e42b
Fabián Ezequiel Gallina <galli.87@gmail.com>
2013-03-13 00:35:47 -0300

    Frame title enhancements.
      + Add "Emacs - " at the beginning of title.
      + Do not attemp emms code if it's not loaded.

--- startup.d/99appareance.el
+++ startup.d/99appareance.el
@@ -19,11 +19,12 @@
 (setq-default
  frame-title-format
  '(:eval
-   (format "%s@%s:%s"
+   (format "Emacs - %s@%s:%s"
            (or (file-remote-p default-directory 'user) user-login-name)
            (or (file-remote-p default-directory 'host) system-name)
            (or (buffer-file-name)
-               (and (member (current-buffer) (emms-playlist-buffer-list))
+               (and (fboundp 'emms-playlist-buffer-list)
+                    (member (current-buffer) (emms-playlist-buffer-list))
                     (emms-playlist-current-selected-track)
                     (emms-track-description
                      (emms-playlist-current-selected-track)))
