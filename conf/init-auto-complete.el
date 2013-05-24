cae751600ccd0fa1ae6e0437c683257c98b1b9e3
Fabián Ezequiel Gallina <galli.87@gmail.com>
2013-05-24 18:06:58 -0300

    Enhancements on auto-complete start-up

--- conf/init-auto-complete.el
+++ conf/init-auto-complete.el
@@ -1,13 +1,10 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/el-get/auto-complete/dict")
 
-(defun ac-common-setup ()
-  (add-to-list 'ac-sources 'ac-source-filename))
-
-(defun ac-emacs-lisp-mode-setup ()
-  (setq ac-sources (append '(ac-source-features ac-source-functions ac-source-variables ac-source-symbols) ac-sources)))
-
-(defun ac-cc-mode-setup ()
-  (setq ac-sources (append '(ac-source-gtags) ac-sources)))
-
 (ac-config-default)
+
+(setq-default ac-sources '(ac-source-yasnippet
+                           ac-source-filename
+                           ac-source-abbrev
+                           ac-source-dictionary
+                           ac-source-words-in-same-mode-buffers))
