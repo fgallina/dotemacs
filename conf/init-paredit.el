421750d513b104c1d328a55f569d189164377106
Fabián Ezequiel Gallina <galli.87@gmail.com>
2013-08-22 20:35:46 -0300

    paredit: enable in more sensible modes

--- conf/init-paredit.el
+++ conf/init-paredit.el
@@ -1,3 +1,4 @@
 (dolist (hook '(clojure-mode-hook emacs-lisp-mode-hook lisp-mode-hook
-                nrepl-mode-hook))
+                nrepl-mode-hook eval-expression-minibuffer-setup-hook
+                ielm-mode-hook lisp-interaction-mode-hook scheme-mode-hook))
   (add-hook hook 'enable-paredit-mode))
