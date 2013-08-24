6eaa5c58d95ea073479bc67ad422e969cf970547
Fabián Ezequiel Gallina <galli.87@gmail.com>
2013-08-24 11:23:13 -0300

    Require multi-web-mode at its init file
    This should not be necessary as proper autoloads exists but for some
    reason it is failing.

--- conf/init-multi-web-mode.el
+++ conf/init-multi-web-mode.el
@@ -1,3 +1,4 @@
+(require 'multi-web-mode)
 (setq mweb-default-major-mode 'html-mode)
 (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
                   (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
