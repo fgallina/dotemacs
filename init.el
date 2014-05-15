f24e788a36f713c53d568e38863b4e621e8307cf
Fabián Ezequiel Gallina <galli.87@gmail.com>
2014-05-14 23:44:12 -0300

    More multiple cursors commands to region bindings
    Added bindings to `region-bindings-mode-map' for
    `mc/skip-to-previous-like-this' and `mc/skip-to-next-like-this' to "P"
    and "N" respectively.

--- init.el
+++ init.el
@@ -834,6 +834,8 @@ MAX-DEPTH limits the depth of subdirectory search."
   :ensure region-bindings-mode
   :config (progn
             (define-key region-bindings-mode-map "a" 'mc/mark-all-like-this)
+            (define-key region-bindings-mode-map "P" 'mc/skip-to-previous-like-this)
+            (define-key region-bindings-mode-map "N" 'mc/skip-to-next-like-this)
             (define-key region-bindings-mode-map "p" 'mc/mark-previous-like-this)
             (define-key region-bindings-mode-map "n" 'mc/mark-next-like-this)
             (define-key region-bindings-mode-map "P" 'mc/unmark-previous-like-this)
