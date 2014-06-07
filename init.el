7f7d3758b18a7d683cfa4630c09ebf958292278e
Fabián Ezequiel Gallina <galli.87@gmail.com>
2014-06-07 17:02:55 -0300

    Remove term customizations

--- init.el
+++ init.el
@@ -996,57 +996,6 @@ MAX-DEPTH limits the depth of subdirectory search."
             (user-package sunrise-x-loop
               :ensure sunrise-x-loop)))
 
-(user-package term
-  :config (progn
-            ;; Based on:
-            ;; http://www.enigmacurry.com/2008/12/26/emacs-ansi-term-tricks/
-            (defun ansi-term-visit-dwim ()
-              "Visit ansi-term Doing What I Mean
-If the current buffer is:
-  1) a running ansi-term named *ansi-term*, rename it.
-  2) a stopped ansi-term, kill it and create a new one.
-  3) a non ansi-term, go to an already running ansi-term
-     or start a new one while killing a defunt one"
-              (interactive)
-              (let ((is-term (string= "term-mode" major-mode))
-                    (is-running (term-check-proc (buffer-name)))
-                    (anon-term (get-buffer "*ansi-term*"))
-                    (program (or explicit-shell-file-name
-                                 (getenv "ESHELL")
-                                 (getenv "SHELL")
-                                 "/bin/sh")))
-                (if is-term
-                    (if is-running
-                        (if (string= "*ansi-term*" (buffer-name))
-                            (let ((newname
-                                   (format "*ansi-term: %s*"
-                                           (read-buffer "Rename buffer (to new name): "))))
-                              (rename-buffer newname))
-                          (if anon-term
-                              (switch-to-buffer "*ansi-term*")
-                            (ansi-term program)))
-                      (kill-buffer (buffer-name))
-                      (ansi-term program))
-                  (if anon-term
-                      (if (term-check-proc "*ansi-term*")
-                          (switch-to-buffer "*ansi-term*")
-                        (kill-buffer "*ansi-term*")
-                        (ansi-term program))
-                    (ansi-term program)))))
-
-            (defun my:term-mode-hook ()
-              (make-local-variable 'mouse-yank-at-point)
-              (make-local-variable 'transient-mark-mode)
-              (auto-fill-mode -1)
-              (setq mouse-yank-at-point t
-                    term-scroll-to-bottom-on-output nil
-                    term-scroll-show-maximum-output nil
-                    term-buffer-maximum-size 2048))
-
-            (add-hook 'term-mode-hook 'my:term-mode-hook)
-
-            (bind-key "<f2>" #'ansi-term-visit-dwim)))
-
 (user-package tex-site
   :ensure auctex
   :defines (latex-help-cmd-alist latex-help-file)
