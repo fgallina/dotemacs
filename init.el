a5f44e23bb9cf2a3af0a1fb7c780750260d2205c
Fabián Ezequiel Gallina <galli.87@gmail.com>
2014-11-22 22:34:41 -0300

    Added helm-flycheck

--- init.el
+++ init.el
@@ -449,31 +449,36 @@ adding files."
 
 (user-package flycheck
   :ensure flycheck
-  :config (progn
-            ;; Add virtualenv support for checkers
-            (defadvice flycheck-check-executable
-              (around python-flycheck-check-executable (checker)
-                      activate compile)
-              "`flycheck-check-executable' with virtualenv support."
-              (if (eq major-mode 'python-mode)
-                  (let* ((process-environment (python-shell-calculate-process-environment))
-                         (exec-path (python-shell-calculate-exec-path)))
-                    ad-do-it)
-                ad-do-it))
+  :config
+  (progn
+    ;; Add virtualenv support for checkers
+    (defadvice flycheck-check-executable
+      (around python-flycheck-check-executable (checker)
+              activate compile)
+      "`flycheck-check-executable' with virtualenv support."
+      (if (eq major-mode 'python-mode)
+          (let* ((process-environment (python-shell-calculate-process-environment))
+                 (exec-path (python-shell-calculate-exec-path)))
+            ad-do-it)
+        ad-do-it))
 
-            (defadvice flycheck-start-checker
-              (around python-flycheck-start-checker (checker)
-                      activate compile)
-              "`flycheck-start-checker' with virtualenv support."
-              (if (eq major-mode 'python-mode)
-                  (let* ((process-environment (python-shell-calculate-process-environment))
-                         (exec-path (python-shell-calculate-exec-path)))
-                    ad-do-it)
-                ad-do-it))
+    (defadvice flycheck-start-checker
+      (around python-flycheck-start-checker (checker)
+              activate compile)
+      "`flycheck-start-checker' with virtualenv support."
+      (if (eq major-mode 'python-mode)
+          (let* ((process-environment (python-shell-calculate-process-environment))
+                 (exec-path (python-shell-calculate-exec-path)))
+            ad-do-it)
+        ad-do-it))
 
-            (setq flycheck-mode-line-lighter " ")
+    (setq flycheck-mode-line-lighter " ")
 
-            (global-flycheck-mode 1)))
+    (global-flycheck-mode 1)
+
+    (user-package helm-flycheck
+      :ensure helm-flycheck)
+    (bind-key "C-c ! !" #'helm-flycheck flycheck-mode-map)))
 
 (user-package gist
   :if (not noninteractive)
